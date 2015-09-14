d03ec63d8b5b90cbb39b64f9c32e9784bd1d3107
acoross <acoross@gmail.com>
2015-09-14 23:51:54 +0900

    regex

--- RecurrentDescentParser/main.cpp
+++ RecurrentDescentParser/main.cpp
@@ -3,24 +3,48 @@
 #include <string>
 #include <regex>
 
-#include "RDParser.h"
+#include "Scanner.h"
+
+
+void RegexSample()
+{
+	std::string s = "Some people, 0192 23222 with a problem, think "
+		"\"I know, I'll use 1123 expressions.\" "
+		"Now they have two problems.";
+
+	std::regex word_regex("[1-9]([0-9]*)");
+	auto words_begin =
+		std::sregex_iterator(s.begin(), s.end(), word_regex);
+	auto words_end = std::sregex_iterator();
+
+	std::cout << "Found "
+		<< std::distance(words_begin, words_end)
+		<< " words\n";
+
+	for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+		std::smatch match = *i;
+		std::string match_str = match.str();
+		std::cout << "  " << match_str << '\n';
+	}
+}
 
 int main()
 {
-	wchar_t* input = L"haha 12322 12dd id3d .83k  ( dfd dddd)";
+	wchar_t* input = L" dfdg 12322 12dd id3d ,83k(dfd dddd)";
 	std::wstring buf = input;
 
 	std::wcout << L"input: " << input << std::endl;
 
-	Token ret;
+	FuncScanner::Token ret;
+	FuncScanner scanner;
 
-	ret = Scan(input);
-	std::wcout << TokenToName(ret.type) << L":" << ret.data << std::endl;
+	ret = scanner.Scan(input);
+	std::wcout << "ret - " << FuncScanner::Token::TokenToName(ret.type) << L":" << ret.data << std::endl << std::endl;
 
-	while (ret.type != TK_NULL)
+	while (ret.type != FuncScanner::TK_NULL)
 	{
-		ret = Scan(input);
-		std::wcout << TokenToName(ret.type) << L":" << ret.data << std::endl;
+		ret = scanner.Scan(input);
+		std::wcout << "ret - " << FuncScanner::Token::TokenToName(ret.type) << L":" << ret.data << std::endl << std::endl;
 	}
 
 	system("pause");
